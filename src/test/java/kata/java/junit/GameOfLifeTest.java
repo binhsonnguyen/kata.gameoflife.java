ef29ca90cda2be5d27b14f674badee0cf14696c4
edward <son@binhsonnguyen.com>
2020-09-24 15:23:52 +0700

    test foundsOneLivingNeighboursAtNextLeft

--- src/test/java/kata/java/junit/GameOfLifeTest.java
+++ src/test/java/kata/java/junit/GameOfLifeTest.java
@@ -26,4 +26,11 @@ public class GameOfLifeTest {
         game.setLiving(1, 3);
         assertEquals(1, game.countLivingNeighbors(1, 4));
     }
+
+    @Test
+    void foundsOneLivingNeighboursAtNextLeft() {
+        game.setLiving(1, 1);
+        game.setLiving(1, 3);
+        assertEquals(1, game.countLivingNeighbors(1, 4));
+    }
 }
