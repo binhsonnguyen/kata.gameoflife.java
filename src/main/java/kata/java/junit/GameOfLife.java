b5252f382afee0cddd1db5e605a00120be01f842
edward <son@binhsonnguyen.com>
2020-09-24 14:32:24 +0700

    pass foundsOneLivingNeighbour

--- src/main/java/kata/java/junit/GameOfLife.java
+++ src/main/java/kata/java/junit/GameOfLife.java
@@ -1,15 +1,17 @@
 package kata.java.junit;
 
 public class GameOfLife {
+    private boolean hasLivingCell = false;
+
     public GameOfLife(int rows, int cols) {
 
     }
 
     public int countLivingNeighbors(int row, int col) {
-        return 0;
+        return hasLivingCell ? 1 : 0;
     }
 
     public void setLiving(int row, int col) {
-
+        hasLivingCell = true;
     }
 }
