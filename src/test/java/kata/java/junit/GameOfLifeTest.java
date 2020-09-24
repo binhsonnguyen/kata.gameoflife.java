b8553b72ba407bce94c4d72db3a9880365c4a020
edward <son@binhsonnguyen.com>
2020-09-24 15:19:42 +0700

    setup initGame method

--- src/test/java/kata/java/junit/GameOfLifeTest.java
+++ src/test/java/kata/java/junit/GameOfLifeTest.java
@@ -1,5 +1,6 @@
 package kata.java.junit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class GameOfLifeTest {
+    private GameOfLife game;
+
+    @BeforeEach
+    void initGame() {
+        game = new GameOfLife(4, 8);
+    }
+
     @Test
     void foundsNoLivingNeighborsInAnEmptyUniverse() {
-        GameOfLife game = new GameOfLife(4, 8);
         assertEquals(0, game.countLivingNeighbors(1, 3));
     }
 
     @Test
     void foundsOneLivingNeighbour() {
-        GameOfLife game = new GameOfLife(4, 8);
         game.setLiving(1, 3);
         assertEquals(1, game.countLivingNeighbors(1, 4));
     }
